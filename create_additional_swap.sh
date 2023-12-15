d1d4c6adb0654de99f8c59c50b8025df2b1c4e3a
Lalatendu Keshari Swain <lkswain_2003@yahoo.com>
2023-12-15 13:42:10 +0530

    Create create_additional_swap.sh

---
+++ create_additional_swap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the swap file path and size
+SWAPFILE_PATH="/swapfile2"
+SWAPFILE_SIZE="1G"  # Adjust the size as needed
+
+# Create the swap file
+sudo fallocate -l $SWAPFILE_SIZE $SWAPFILE_PATH
+sudo chmod 600 $SWAPFILE_PATH
+sudo mkswap $SWAPFILE_PATH
+sudo swapon $SWAPFILE_PATH
+
+# Update /etc/fstab for persistence
+echo "$SWAPFILE_PATH none swap sw 0 0" | sudo tee -a /etc/fstab
+
+echo "Additional swap file created and activated."
